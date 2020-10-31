5b28129724fc27baa0b90f170c904cfdecb9ea1c
dgwalkerii1231 <70613928+dgwalkerii1231@users.noreply.github.com>
2020-10-30 20:21:15 -0700

    Initial commit

---
+++ PlayerViewController.swift
@@ -0,0 +1,17 @@
+//
+//  PlayerViewController.swift
+//  MyMovies10
+//
+//  Created by Danny Walker on 10/30/20.
+//
+
+import UIKit
+import AVKit
+import AVFoundation
+
+
+class PlayerViewController: AVPlayerViewController {
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+            return .landscapeLeft
+        }
+}
